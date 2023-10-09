a6764db28e0c686496b261d6c871e2725f483cfe
Daniel <daniel.sperber@mail.uni-mannheim.de>
2023-10-09 23:24:11 +0200

    Using new interface & highway examples

--- Vehicle.py
+++ Vehicle.py
@@ -2,12 +2,15 @@ import carla
 
 
 class Vehicle:
-    def __init__(self, world, make):
+    def __init__(self, world, make=""):
         self.actor = None
         self.world = world
         self.control = carla.VehicleControl()
         blueprint_library = world.get_blueprint_library()
-        self.actorBlueprint = blueprint_library.filter(make)[0]
+        if isinstance(make, carla.ActorBlueprint):
+            self.actorBlueprint = make
+        else:
+            self.actorBlueprint = blueprint_library.filter(make)[0]
 
     def spawn(self, transform):
         self.actor = self.world.spawn_actor(self.actorBlueprint, transform)
