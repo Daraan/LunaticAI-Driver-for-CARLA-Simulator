06c6c05b38c4ee0a846149141dc7b3209d68ee00
Bogdan Oprisiu <bobo.oprisiu@gmail.com>
2023-10-10 00:48:13 +0300

    Revert "distance to car ahead and behind"
    This reverts commit 76d72c1a910293e58488cbb2d35b7b6db603d1f3.

--- Vehicle.py
+++ Vehicle.py
@@ -1,14 +1,4 @@
-from cmath import sqrt
-
 import carla
-
-
-def calculateDistance(location1, location2):
-    return sqrt(
-        (location1.x ** 2 - location2.x ** 2) +
-        (location1.y ** 2 - location2.y ** 2) +
-        (location1.z ** 2 - location2.z ** 2)
-    ).real
 
 
 class Vehicle:
@@ -18,13 +8,6 @@ class Vehicle:
         self.control = carla.VehicleControl()
         blueprint_library = world.get_blueprint_library()
         self.actorBlueprint = blueprint_library.filter(make)[0]
-
-    def __eq__(self, other):
-        return not (
-                self.getLocation().x != other.getLocation().x
-                or self.getLocation().y != other.getLocation().y
-                or self.getLocation().z != other.getLocation().z
-        )
 
     def spawn(self, transform):
         self.actor = self.world.spawn_actor(self.actorBlueprint, transform)
@@ -50,46 +33,3 @@ class Vehicle:
     def setHandbrake(self, value):
         self.control.hand_brake = value
         self.actor.apply_control(self.control)
-
-    def getLocation(self):
-        return self.actor.get_transform().location
-
-    def findCarBehind(self, vehicles: []):
-        closestCar = None
-        for car in vehicles:
-            if (
-                    car != self
-                    and self.getLocation().y < car.getLocation().y
-                    and (closestCar is None or car.getLocation().y < closestCar.getLocation().y)
-            ):
-                closestCar = car
-
-        return closestCar
-
-    def findCarAhead(self, vehicles: []):
-        closestCar = None
-        for car in vehicles:
-            if (
-                    car != self
-                    and car.getLocation().y > self.getLocation().y
-                    and (closestCar is None or car.getLocation().y < closestCar.getLocation().y)
-            ):
-                closestCar = car
-
-        return closestCar
-
-    def distanceToCarAhead(self, vehicles: []):
-        vehicleAhead = self.findCarAhead(vehicles)
-        if vehicleAhead is None:
-            return None
-        locationAhead = vehicleAhead.actor.get_transform().location
-
-        return calculateDistance(self.getLocation(), locationAhead)
-
-    def distanceToCarBehind(self, vehicles: []):
-        vehicleBehind = self.findCarBehind(vehicles)
-        if vehicleBehind is None:
-            return None
-        locationBehind = vehicleBehind.actor.get_transform().location
-
-        return calculateDistance(self.getLocation(), locationBehind)
