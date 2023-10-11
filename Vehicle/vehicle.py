f82e8615b80948f7119a88dac38a41e4914271cd
Bogdan Oprisiu <bobo.oprisiu@gmail.com>
2023-10-11 12:21:37 +0300

    Add Wrapper for carla.Vehicle and our custom vehicle class

--- Vehicle/vehicle.py
+++ Vehicle/vehicle.py
@@ -3,6 +3,7 @@ from cmath import sqrt
 import carla
 from carla import Vector3D
 
+
 def calculateDistance(location1, location2):
     return sqrt(
         (location1.x ** 2 - location2.x ** 2) +
@@ -10,24 +11,26 @@ def calculateDistance(location1, location2):
         (location1.z ** 2 - location2.z ** 2)
     ).real
 
+
 from carla import Vector3D
+
 
 class Vehicle:
     # TODO would be nice if we could derive this from carla.Vehicle
     # should be able to use some functions directly without using functions for all
 
-    instances : list = [] # for easier destruction later
+    instances: list = []  # for easier destruction later
 
-    def __init__(self, world : carla.World, make=""):
-        self.actor : carla.Actor = None
+    def __init__(self, world: carla.World, make=""):
+        self.actor: carla.Actor = None
         self.world = world
-        self.control : carla.VehicleControl = carla.VehicleControl()
+        self.control: carla.VehicleControl = carla.VehicleControl()
         blueprint_library = world.get_blueprint_library()
         if isinstance(make, carla.ActorBlueprint):
-            self.actorBlueprint : carla.ActorBlueprint = make
+            self.actorBlueprint: carla.ActorBlueprint = make
         else:
-            self.actorBlueprint : carla.ActorBlueprint = blueprint_library.filter(make)[0]
-        Vehicle.instances.append(self) # access all instances over the class
+            self.actorBlueprint: carla.ActorBlueprint = blueprint_library.filter(make)[0]
+        Vehicle.instances.append(self)  # access all instances over the class
 
     def __eq__(self, other):
         return not (
@@ -36,9 +39,8 @@ class Vehicle:
                 or self.getLocation().z != other.getLocation().z
         )
 
-
     def spawn(self, transform):
-        self.actor : carla.Actor = self.world.spawn_actor(self.actorBlueprint, transform)
+        self.actor: carla.Actor = self.world.spawn_actor(self.actorBlueprint, transform)
         self.actor.apply_control(self.control)
 
     def focusCamera(self):
@@ -49,7 +51,7 @@ class Vehicle:
         self.control.throttle = value
         self.actor.apply_control(self.control)
 
-    def setVelocity(self, speed : Vector3D =3):
+    def setVelocity(self, speed: Vector3D = 3):
         if not isinstance(speed, Vector3D):
             self.actor.enable_constant_velocity(Vector3D(x=speed))
         else:
---
+++ Vehicle/vehicleWrapper.py
@@ -0,0 +1,60 @@
+import carla
+from carla import Vector3D
+from Vehicle.vehicle import Vehicle
+
+
+class VehicleWrapper:
+    # Wrap our own Vehicle class and the CARLA vehicle class
+
+    instances: list = []  # for easier destruction later
+
+    def __init__(self, world: carla.World, make=""):
+        self.vehicle = Vehicle(world, make)
+
+    def __getattr__(self, attr):
+        # Delegate attribute access to the CARLA Vehicle class
+        if hasattr(self.vehicle.actor, attr):
+            return getattr(self.vehicle.actor, attr)
+        raise AttributeError(f"'Invalid attribute {attr}'{attr}'")
+
+    def getCarlaVehicle(self):
+        return self.carlaVehicle
+
+    def __eq__(self, other):
+        return self.vehicle == other.vehicle
+
+    def spawn(self, transform):
+        self.vehicle.spawn(transform)
+
+    def focusCamera(self):
+        self.vehicle.focusCamera()
+
+    def setThrottle(self, value):
+        self.vehicle.setThrottle(value)
+
+    def setVelocity(self, speed: Vector3D = 3):
+        self.vehicle.setVelocity(speed)
+
+    def setBrake(self, value):
+        self.vehicle.setBrake(value)
+
+    def setSteering(self, value):
+        self.vehicle.setSteering(value)
+
+    def setHandbrake(self, value):
+        self.vehicle.setHandbrake(value)
+
+    def getLocation(self):
+        return self.vehicle.getLocation()
+
+    def findCarBehind(self, vehicles: []):
+        return self.vehicle.findCarBehind(vehicles)
+
+    def findCarAhead(self, vehicles: []):
+        return self.vehicle.findCarAhead(vehicles)
+
+    def distanceToCarAhead(self, vehicles: []):
+        return self.vehicle.distanceToCarAhead(vehicles)
+
+    def distanceToCarBehind(self, vehicles: []):
+        return self.vehicle.distanceToCarBehind(vehicles)
