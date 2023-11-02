9c54c461b2a828d72702fcdf638fadf75e9aa99b
Daniel <github.blurry@9ox.net>
2023-11-02 19:45:50 +0100

    Optional Parameters to CarlaService

--- classes/carla_service.py
+++ classes/carla_service.py
@@ -5,11 +5,12 @@ from classes.vehicle import Vehicle
 
 
 class CarlaService:
-    def __init__(self, worldName, ip, port):
+    def __init__(self, worldName=None, ip="localhost", port="2000"):
         self.client = carla.Client(ip, port)
         self.client.set_timeout(2.0)
         self.vehicleList = []
-        # self.client.load_world(worldName)
+        if worldName:
+            self.client.load_world(worldName)
         self.world = self.client.get_world()
 
     def createCar(self, model):
