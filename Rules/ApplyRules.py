f879cb366dd66653120d652002a5730b94123a7d
Bogdan Oprisiu <bobo.oprisiu@gmail.com>
2023-12-13 21:23:41 +0200

    It actually works now :)

--- Rules/ApplyRules.py
+++ Rules/ApplyRules.py
@@ -2,8 +2,6 @@ import builtins
 import importlib
 import json
 
-import yaml
-
 
 class RuleInterpreter:
     def __init__(self, filename):
@@ -21,19 +19,20 @@ class RuleInterpreter:
         if file_extension == 'json':
             with open(filename, 'r') as file:
                 return json.load(file)
-        elif file_extension == 'yaml' or file_extension == 'yml':
-            with open(filename, 'r') as file:
-                return yaml.safe_load(file)
         else:
-            raise ValueError("Unsupported file format. Please provide a JSON or YAML file.")
+            raise ValueError("Unsupported file format. Please provide a JSON file.")
 
     def execute_all_functions(self, driver, matrix, i_car, j_car, tm):
         results = {}
         for function_name, function_data in self.functions.items():
-            if 'logic' in function_data:
+            try:
                 logic = compile(function_data['logic'], '', 'exec')
                 local_vars = {**locals().copy(), **function_data.get('optional_parameters', {})}
                 exec(logic, globals(), local_vars)
-                func_result = locals()[function_name](driver, matrix, i_car, j_car, tm)
-                results[function_name] = func_result if func_result is not None else "No return value"
+                func = local_vars.get(function_name)
+                if callable(func):
+                    func_result = func(driver, matrix, i_car, j_car, tm)
+                    results[function_name] = func_result if func_result is not None else "No return value"
+            except Exception as e:
+                results[function_name] = f"Error executing function: {e}"
         return results
