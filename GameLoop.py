6086f9baf9d35431f8bfca8d57fa5061153f1677
Bogdan Oprisiu <bobo.oprisiu@gmail.com>
2023-12-22 11:17:19 +0200

    Vehicle Spawner WIP

--- GameLoop.py
+++ GameLoop.py
@@ -5,44 +5,36 @@ from asyncio import Queue
 
 import carla
 
-from CarlaFunctions import *
 from DataGathering.informationUtils import get_all_road_lane_ids
 from DataGathering.matrix_wrap import get_car_coords
 from DataGathering.run_matrix import DataMatrix
+from VehicleSpawning.vehicle_spawner import VehicleSpawner
 from utils.Camera import camera_function
 
+spawner = None
 vehicles = []
 
 
 def main():
-    global client, i_car, j_car, vehicles
-    # Usage of functions from CarlaFunctions.py
-    # Initialize the Carla service
-    carla_service = initialize_carla_service()
+    global spawner, vehicles
 
-    # Set up the world
-    world, world_map = setup_world(carla_service)
+    # Initialise the class for vehicle spawning
+    spawner = VehicleSpawner('VehicleSpawning/config/vehicle_spawn.yaml')
+    spawner.initialize_carla_service()
+    world, world_map = spawner.setup_world()
+    ego_bp, car_bp, driver1, spawn_points, rule_interpreter = spawner.prepare_vehicles(world)
 
-    driver1 = Driver("config/insane_driver.json", traffic_manager=client)
-    driver2 = Driver("config/aggressive_driver.json", traffic_manager=client)
-    driver3 = Driver("config/default_driver.json", traffic_manager=client)
-    # Prepare vehicles
-    ego_bp, car_bp, driver1, spawn_points, rule_interpreter = prepare_vehicles(world)
-
-    # Spawn vehicles
-    ego = spawn_vehicles(world, ego_bp, car_bp)
-
-    # Assign drivers
-    ego_vehicle = assign_drivers(carla_service, ego, driver1)
+    # Spawn vehicles and assign drivers
+    ego = spawner.spawn_vehicles(world, ego_bp, spawn_points)
+    ego_vehicle = spawner.assign_drivers(ego, driver1)
 
     # Spawn traffic
-    vehicles, tm = spawn_traffic(world, car_bp, spawn_points, driver1, ego_vehicle)
+    vehicles, tm = spawner.spawn_traffic(world, car_bp, spawn_points, driver1, ego.vehicle)
 
     # Initialize loop variables
     world.tick()
     road_lane_ids = get_all_road_lane_ids(world_map=world.get_map())
     t_end = time.time() + 10000
-    i_car, j_car = 0, 0
 
     # Create a thread for the camera functionality
     camera_thread = threading.Thread(target=camera_function, args=(ego_vehicle, world))
@@ -53,33 +45,15 @@ def main():
 
     while time.time() < t_end:
         try:
-            # matrix = wrap_matrix_functionalities(ego_vehicle, world, world_map, road_lane_ids)
-
+            pass
             # Retrieve the latest matrix from the matrix thread
             matrix = data_matrix.getMatrix()
 
             if matrix is None:
-                # time.sleep(0.1)
                 continue
 
-            # print(matrix)
-            ego_location = ego_vehicle.get_location()
-            ego_waypoint = world_map.get_waypoint(ego_location)
-
             (i_car, j_car) = get_car_coords(matrix)
-
-            # random brake check
-            brake_check_choice = random.randint(1, 100)
-            if (brake_check_choice <= driver1.brake_check_chance
-                    and (matrix[i_car][j_car - 1] == 2)
-            ):
-                driver1.vehicle.actor.set_autopilot(False)
-                driver1.vehicle.setThrottle(0)
-                driver1.vehicle.setBrake(10)
-                time.sleep(1.0)
-                print("Brake check")
-                driver1.vehicle.setBrake(0)
-                driver1.vehicle.actor.set_autopilot(True)
+            results = rule_interpreter.execute_all_functions(driver1, matrix, i_car, j_car, tm)
 
             overtake_direction = 0
             # Random lane change
@@ -131,7 +105,7 @@ if __name__ == '__main__':
         main()
     finally:
         try:
-            client.apply_batch([carla.command.DestroyActor(x.actor) for x in vehicles])
+            if spawner and spawner.client:
+                spawner.client.apply_batch([carla.command.DestroyActor(x.actor) for x in vehicles])
         except NameError:
-            # Should be client not defined
             pass
