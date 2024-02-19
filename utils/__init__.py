82485863d2f4ca608142f140b05b71868be95e77
Daniel <github.blurry@9ox.net>
2024-02-19 16:33:31 +0100

    fix: added carla import to utils to avoid import error

--- utils/__init__.py
+++ utils/__init__.py
@@ -1,3 +1,9 @@
+try:
+    import carla
+except ImportError as e:
+    from utils.egg_import import import_carla
+    carla = import_carla()
+
 from . import argument_parsing
 from . import blueprint_helpers
 from . import general
