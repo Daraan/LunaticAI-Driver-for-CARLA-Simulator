d014300a4bd06cacf8412a0a10c8f6f5a31e6c3b
Daniel <github.blurry@9ox.net>
2024-02-25 18:06:06 +0100

    Updated comments

--- utils/Camera.py
+++ utils/Camera.py
@@ -1,13 +1,39 @@
 import carla
 
-def follow_car(ego_vehicle, world : carla.World):
+def follow_car(ego_vehicle : carla.Actor, world : carla.World):
+    """
+    Set the spectator's view to follow the ego vehicle.
+
+    Parameters:
+        ego_vehicle (Vehicle): The ego vehicle that the spectator will follow.
+        world (World): The game world where the spectator view is set.
+
+    Description:
+        This function calculates the desired spectator transform by positioning the spectator
+        10 meters behind the ego vehicle and 5 meters above it. The spectator's view will follow
+        the ego vehicle from this transformed position.
+
+    Note:
+        To face the vehicle from behind, uncomment the line 'spectator_transform.rotation.yaw += 180'.
+
+    Returns:
+        None: The function does not return any value.
+    """
+    # Calculate the desired spectator transform
     vehicle_transform = ego_vehicle.get_transform()
     spectator_transform = vehicle_transform
-    spectator_transform.location -= (vehicle_transform.get_forward_vector() * 10)
-    spectator_transform.location += (vehicle_transform.get_up_vector() * 5)
+    spectator_transform.location -= (
+        vehicle_transform.get_forward_vector() * 10
+    )  # Move 10 meters behind the vehicle
+    spectator_transform.location += (
+        vehicle_transform.get_up_vector() * 5
+    )  # Move 5 meters above the vehicle
+    # spectator_transform.rotation.yaw += 180 # Face the vehicle from behind
+
+    # Set the spectator's transform in the world
     world.get_spectator().set_transform(spectator_transform)
 
 
-def camera_function(ego_vehicle, world):
+def camera_function(ego_vehicle : carla.Actor, world : carla.World):
     while True:
         follow_car(ego_vehicle, world)
