0bc7d69415ab2ae27f67c9f2ed7b38084dc4ec5c
Daniel <github.blurry@9ox.net>
2024-03-19 16:10:00 +0100

    Addeed & Changed to Town10 route

--- run_leaderboard_agent.sh
+++ run_leaderboard_agent.sh
@@ -1,7 +1,6 @@
 export LEADERBOARD_ROOT=~/TeamProject/leaderboard
 
 # Parameterization settings. These will be explained in 2.2. Now simply copy them to run the test.
-export ROUTES=${LEADERBOARD_ROOT}/data/routes_devtest.xml
 export REPETITIONS=1
 export DEBUG_CHALLENGE=1
 export CHECKPOINT_ENDPOINT=${LEADERBOARD_ROOT}/results.json
@@ -14,6 +13,13 @@ export TEAM_CONFIG=${TEAM_AGENT_ROOT}/conf/launch_config.yaml
 
 export SCENARIO_RUNNER_ROOT=${TEAM_AGENT_ROOT}/scenario_runner
 export PYTHONPATH=${TEAM_AGENT_ROOT}:${LEADERBOARD_ROOT}:${SCENARIO_RUNNER_ROOT}:${PYTHONPATH}
+
+# Choose Route
+
+# Alternative Route for faster testing on Town 10
+export ROUTES=${SCENARIO_RUNNER_ROOT}/srunner/data/routes_town10.xml
+# Town 12 Devtest Route
+#export ROUTES=${LEADERBOARD_ROOT}/data/routes_devtest.xml
 
 ${LEADERBOARD_ROOT}/scripts/run_evaluation.sh 
 
