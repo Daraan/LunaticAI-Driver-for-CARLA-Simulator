c85d99075e90702dafacef2ac93fea41bb74208b
Daniel <github.blurry@9ox.net>
2024-03-19 12:54:54 +0100

    Cloned current leaderboard agent state

--- agents/leaderboard_agent.py
+++ agents/leaderboard_agent.py
@@ -15,16 +15,32 @@ from leaderboard.autoagents.autonomous_agent import Track
 
 from srunner.scenariomanager.carla_data_provider import CarlaDataProvider
 
-def get_entry_point():
-    print("Getting entry point")
-    return "LunaticChallenger"
 
 hydra_initalized = False
 import logging
 logger.setLevel(logging.DEBUG)
 
-args: LaunchConfig 
 
+def get_entry_point():
+    print("Getting entry point")
+    return "LunaticChallenger"
+
+# TODO: Pack this in an extra config
+WORLD_MODEL_DESTROY_SENSORS = False
+ENABLE_RSS = False
+ENABLE_DATA_MATRIX = True
+
+DATA_MATRIX_ASYNC = True
+DATA_MATRIX_TICK_SPEED = 60
+
+class UserInterruption(Exception):
+    """
+    Terminate the run_step loop if user input is detected.
+    
+    Allow the scenario runner and leaderboard to exit gracefully.
+    """
+
+args: LaunchConfig 
 class LunaticChallenger(AutonomousAgent, LunaticAgent):
     
     def __init__(self, carla_host, carla_port, debug=False):
@@ -46,8 +62,15 @@ class LunaticChallenger(AutonomousAgent, LunaticAgent):
                                     job_name="test_app")
             args = compose(config_name=config_name)
             hydra_initalized = True
-            args.map = None # Let scenario manager decide
+            # Let scenario manager decide
+            args.map = None
             args.sync = None
+            args.handle_ticks = False # Assure that this is false
+            
+            args.agent.data_matrix.enabled = ENABLE_DATA_MATRIX
+            args.agent.data_matrix.sync = not DATA_MATRIX_ASYNC
+            args.agent.data_matrix.sync_interval = DATA_MATRIX_TICK_SPEED
+            args.agent.rss.enabled = ENABLE_RSS
             print(OmegaConf.to_yaml(args))
         self.args = args
         
@@ -68,26 +91,59 @@ class LunaticChallenger(AutonomousAgent, LunaticAgent):
         LunaticAgent.__init__(self, config, self.world_model, map_inst=map_inst, grp_inst=CarlaDataProvider.get_global_route_planner())
         self.game_framework.agent = self # TODO: Remove this circular reference
         self.agent_engaged = False
+        self._destroyed = False
+        
+    def sensors(self):
+        sensors: list = super().sensors()
+        sensors.extend([
+            {'type': 'sensor.opendrive_map', 'reading_frequency': 1, 'id': 'OpenDRIVE'},
+        ])
+        return sensors
+
+    @staticmethod
+    def _print_input_data(input_data):
+        if not input_data:
+            print("No input data:", input_data)
+            return
+        print("=====================>")
+        for key, val in input_data.items():
+            if hasattr(val[1], 'shape'):
+                shape = val[1].shape
+                print("[{} -- {:06d}] with shape {}".format(key, val[0], shape))
+            else:
+                print("[{} -- {:06d}] ".format(key, val[0]))
+        print("<=====================")
 
     def run_step(self, input_data, timestamp):
         try:
-            print("Running step")
-            print("Input data", input_data)
+            self._print_input_data(input_data)
             self.agent_engaged = True
             with self.game_framework:
                 control = super(AutonomousAgent, self).run_step(debug=self.args.debug) # Call Lunatic Agent run_step
             # Handle render updates
+            
+            self.execute_phase(Phase.APPLY_MANUAL_CONTROLS | Phase.BEGIN, prior_results=control)
+            if self.controller.parse_events(self.get_control()):
+                print("Exiting by user input.")
+                raise UserInterruption("Exiting by user input.")
+            self.execute_phase(Phase.APPLY_MANUAL_CONTROLS | Phase.END, prior_results=None)
+            
             self.execute_phase(Phase.EXECUTION | Phase.BEGIN, prior_results=control)
             return control
         except Exception as e:
-            logger.error("Error in LunaticChallenger.run_step:", exc_info=True)
+            if not isinstance(e, UserInterruption):
+                logger.error("Error in LunaticChallenger.run_step:", exc_info=True)
             self.destroy()
             raise e
     
     def destroy(self):
+        self._destroyed = True
         print("Destroying")
+        self._road_matrix_updater.stop()
         super().destroy()
         if self.world_model:
+            if not WORLD_MODEL_DESTROY_SENSORS:
+                self.world_model.actors.clear()
             self.world_model.destroy()
             self.world_model = None
         if self.game_framework:
@@ -95,3 +151,7 @@ class LunaticChallenger(AutonomousAgent, LunaticAgent):
             self.game_framework = None
         pygame.quit()
         print("Destroyed", self)
+        
+    def __del__(self):
+        if not self._destroyed:
+            self.destroy()
