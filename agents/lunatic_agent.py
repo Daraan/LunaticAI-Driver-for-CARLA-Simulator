b3d933fb3b0228ad0616098beee3a1394a1d1c8f
Daniel <github.blurry@9ox.net>
2024-04-09 12:10:08 +0200

    Added new set_destination to lunatic agent

--- agents/lunatic_agent.py
+++ agents/lunatic_agent.py
@@ -857,7 +857,7 @@ class LunaticAgent(BehaviorAgent):
 
     #def done(self): # from base class self._local_planner.done()
 
-    #def set_destination(self, end_location, start_location=None):
+    def set_destination(self, end_location, start_location=None, clean_queue=True):
         """
         This method creates a list of waypoints between a starting and ending location,
         based on the route returned by the global router, and adds it to the local planner.
@@ -867,6 +867,21 @@ class LunaticAgent(BehaviorAgent):
             :param end_location (carla.Location): final location of the route
             :param start_location (carla.Location): starting location of the route
         """
+        if not start_location:
+            if self._local_planner.target_waypoint:
+                start_location = self._local_planner.target_waypoint.transform.location # waypoint at queue[0] at run_step
+            else:
+                start_location = self._vehicle.get_location()
+            clean_queue = True
+        else:
+            pass
+            #start_location = self._vehicle.get_location()
+            #clean_queue = False
+        start_waypoint = CarlaDataProvider.get_map().get_waypoint(start_location)
+        end_waypoint = CarlaDataProvider._map.get_waypoint(end_location) if isinstance(end_location, carla.Location) else end_location
+
+        route_trace = self.trace_route(start_waypoint, end_waypoint)
+        self._local_planner.set_global_plan(route_trace, clean_queue=clean_queue)
         
     #def set_global_plan(self, plan, stop_waypoint_creation=True, clean_queue=True):
         """
