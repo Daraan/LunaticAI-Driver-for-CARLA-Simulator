0a057ed14e5209025809b30a46d76e387f6c8f1a
Daniel <github.blurry@9ox.net>
2024-06-24 21:17:59 +0200

    Automatically convert records to avi movie
    use ffmpeg

--- agents/tools/config_creation.py
+++ agents/tools/config_creation.py
@@ -1482,7 +1482,7 @@ class CameraConfig(AgentConfig):
         Set at WorldModel level
         """
         
-        output_path : str = '${hydra:runtime.output_dir}/recorderX/session%03d/%08d.bmp'
+        output_path : str = '${hydra:runtime.output_dir}/recorder/session%03d/%08d.bmp'
         """
         Folder to record the camera
         
